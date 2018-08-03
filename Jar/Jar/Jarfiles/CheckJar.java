6530c81be55f38308ca4975c3e0cf8a4482ad8db
Aashishd220 <41561579+Aashishd220@users.noreply.github.com>
2018-08-03 23:49:03 +0530

    Add files via upload

---
+++ Jar/Jar/Jarfiles/CheckJar.java
@@ -0,0 +1,8 @@
+package Jarfiles;
+import Jarfiles.Emp;//importing jar file
+public class CheckJar {
+	public static void main(String[] args) {
+		Emp.display();//calling function of different package
+	}
+	
+}
